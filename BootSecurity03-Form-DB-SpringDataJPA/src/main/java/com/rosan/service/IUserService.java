cd6d947db7b366467dfdc7edd28634d18762f10b
roshanacharya <roshanacharya@pop-os>
2024-10-15 22:47:58 +0545

    ok

--- BootSecurity03-Form-DB-SpringDataJPA/src/main/java/com/rosan/service/IUserService.java
+++
@@ -1,9 +0,0 @@
-package com.rosan.service;
-
-import org.springframework.security.core.userdetails.UserDetailsService;
-
-import com.rosan.model.UserDetails;
-
-public interface IUserService extends UserDetailsService{
-	public String register(UserDetails details);
-}
